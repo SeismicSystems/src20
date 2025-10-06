e3031d9afc85c9308fd2f97daf65fdd02fee6600
Lyron Co Ting Keh <lyronctk@stanford.edu>
2025-10-06 09:16:59 -0400

    Filtering on CLI using indexed keyhash

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -44,21 +44,18 @@ export function parseEncryptedData(encryptedData: Buffer): {
   ciphertext: Buffer;
   nonce: Buffer;
 } {
-  const nonce = encryptedData.slice(- NONCE_LENGTH);
-  const ciphertext = encryptedData.slice(0, - NONCE_LENGTH);
+  const nonce = encryptedData.slice(-NONCE_LENGTH);
+  const ciphertext = encryptedData.slice(0, -NONCE_LENGTH);
 
   return { ciphertext, nonce };
 }
 
 export async function attachEventListener(client: ShieldedWalletClient, aesKey: Buffer) {
+  const keyHash = keccak256(aesKey) as `0x${string}`;
+
   const transferEvent = SRC20Abi.find(
     (item: any) => item.type === 'event' && item.name === 'Transfer'
   ) as AbiEvent;
-  const approvalEvent = SRC20Abi.find(
-    (item: any) => item.type === 'event' && item.name === 'Approval'
-  ) as AbiEvent;
-
-  const keyHash = keccak256(aesKey) as `0x${string}`;
   client.watchEvent({
     address: DeployOut.MockSRC20 as `0x${string}`,
     event: transferEvent,
@@ -67,35 +64,40 @@ export async function attachEventListener(client: ShieldedWalletClient, aesKey: 
     },
     onLogs: (logs: any[]) => {
       logs.forEach(async (log) => {
-        handleEvent(aesKey, log);
+        handleEvent(aesKey, log, logTransfer);
+      });
+    },
+  });
+
+  const approvalEvent = SRC20Abi.find(
+    (item: any) => item.type === 'event' && item.name === 'Approval'
+  ) as AbiEvent;
+  client.watchEvent({
+    address: DeployOut.MockSRC20 as `0x${string}`,
+    event: approvalEvent,
+    args: {
+      encryptKeyHash: keyHash,
+    },
+    onLogs: (logs: any[]) => {
+      logs.forEach(async (log) => {
+        handleEvent(aesKey, log, logApproval);
       });
     },
   });
 }
 
-async function handleEvent(aesKey: Buffer, log: any) {
-  const { args, eventName } = log;
-  const { encryptedAmount } = args;
-  
+async function handleEvent(aesKey: Buffer, log: any, callback: (data: any) => void) {
+  const { encryptedAmount } = log.args;
   const encryptedAmountBuffer = Buffer.from(encryptedAmount.slice(2), 'hex');
   const { ciphertext, nonce } = parseEncryptedData(encryptedAmountBuffer);
-
-  const localKeyHash = keccak256(aesKey);
-  if (localKeyHash === `0x${keyHash.toString('hex')}`) {
-    const amountBytes = decrypt(aesKey, nonce, ciphertext);
-    const amount = BigInt(`0x${amountBytes.toString('hex')}`);
-
-    if (eventName === 'Transfer') {
-      logTransfer(log, amount);
-    }
-    if (eventName === 'Approval') {
-      logApproval(log, amount);
-    }
-  }
+  const amountBytes = decrypt(aesKey, nonce, ciphertext);
+  const amount = BigInt(`0x${amountBytes.toString('hex')}`);
+  
+  callback({...log.args, amount});
 }
 
-function logTransfer(log: any, amount: bigint) {
-  const { from, to } = log.args;
+function logTransfer(data: any) {
+  const { from, to, amount } = data;
 
   console.log('Transfer(address from, address to, bytes encryptedAmount)\n');
   console.log('    from:', from);
@@ -103,8 +105,8 @@ function logTransfer(log: any, amount: bigint) {
   console.log('    amount:', amount, '\n');
 }
 
-function logApproval(log: any, amount: bigint) {
-  const { owner, spender } = log.args;
+function logApproval(data: any) {
+  const { owner, spender, amount } = data;
 
   console.log('Approval(address owner, address spender, bytes encryptedAmount)\n');
   console.log('    owner:', owner);
