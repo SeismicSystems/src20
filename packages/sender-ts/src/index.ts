5a40bf044f0a8f3b9402459b9606fec3240d1ab7
Lyron Co Ting Keh <lyronctk@stanford.edu>
2025-12-05 16:02:06 -0500

    Improved logging in waitForTx

--- packages/sender-ts/src/index.ts
+++ packages/sender-ts/src/index.ts
@@ -29,27 +29,27 @@ async function main() {
     Object.entries(accounts).map(([name, account]) => [name, account.address])
   );
 
-  console.log('Running on on network:', chain.name);
+  console.log('Running on on network:', chain.name, "\n");
 
   // Pay a random amount, cycle from Alice -> Bob -> Charlie -> Alice -> ...
   while (true) {
     const amount = BigInt(Math.floor(Math.random() * (Number(1e18) - 1) + 1));
 
-    console.log('Sending Alice -> Bob:', amount);
+    console.log('Sending Alice -> Bob:', amount, "\n");
     await waitForTx(
       interfaces['alice'].contract.write.transfer([addresses['bob'], amount]),
       interfaces['alice'].client
     );
     await sleep(5000);
 
-    console.log('Sending Bob -> Charlie:', amount);
+    console.log('Sending Bob -> Charlie:', amount, "\n");
     await waitForTx(
       interfaces['bob'].contract.write.transfer([addresses['charlie'], amount]),
       interfaces['bob'].client
     );
     await sleep(5000);
 
-    console.log('Sending Charlie -> Alice:', amount);
+    console.log('Sending Charlie -> Alice:', amount, "\n");
     await waitForTx(
       interfaces['charlie'].contract.write.transfer([addresses['alice'], amount]),
       interfaces['charlie'].client
